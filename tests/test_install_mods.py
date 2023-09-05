60bce942e1835f88f30cbce304a7706e230dd691
Kristian Payne <kristianpayne1@gmail.com>
2023-09-05 22:33:05 +0100

    Fix unable to find servertest.ini

--- tests/test_install_mods.py
+++ tests/test_install_mods.py
@@ -6,4 +6,4 @@ from scripts.install_mods.install_mods import main
 STEAM_URL = os.environ['STEAM_URL']
 
 def test_main():
-    main(STEAM_URL, Path("./test/servertest.ini"))
+    main(STEAM_URL, Path("./tests/servertest.ini"))
